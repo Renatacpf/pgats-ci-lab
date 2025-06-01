ceef56186082f2344d652575ee4302ff90531cf3
Renata França <94009738+Renatacpf@users.noreply.github.com>
2025-06-01 12:50:36 -0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -185,4 +185,12 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+  reporters: [
+    "default",
+    [ "jest-junit", { 
+      outputDirectory: "test-results", 
+      outputName: "results.xml" 
+    }]
+  ],
 };
